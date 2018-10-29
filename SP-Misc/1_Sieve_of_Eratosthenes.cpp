441a7d704be54cec818fd568bc5e3c9a2c2fc00f
Mohamed Bakr <mohamed556d@gmail.com>
2018-10-29 11:23:20 +0200

    Correct Answer

--- SP-Misc/1_Sieve_of_Eratosthenes.cpp
+++ SP-Misc/1_Sieve_of_Eratosthenes.cpp
@@ -12,12 +12,13 @@ int main() {
 	   memset(prime, true, sizeof(prime)); 
 	    for (int p = 2; p*p <= n; p ++){
 	         // If prime[p] is not changed, then it is a prime 
-            if (prime[p] == true) 
-            { 
-                // Update all multiples of p 
-                for (int i=p*2; i<=n; i += p) 
-                    prime[i] = false; 
-            } 
+		    
+	    if (prime[p] == true) 
+	    { 
+		// Update all multiples of p 
+		for (int i=p*2; i<=n; i += p) 
+		    prime[i] = false; 
+	    } 
 	    }
 	   for (int p=2; p<=n; p++) 
             if (prime[p]) 
