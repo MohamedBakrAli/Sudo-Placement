1c362e96a8629b415f4d67b1e23211ed1c319bb3
MohamedBakrAli <mohamed556d@gmail.com>
2018-11-01 12:37:37 +0200

    Correct Answer

---
+++ SP-Misc/6_Maximum Width of Tree.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+#include <queue>
+using namespace std;
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child */
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+/* Function to get the maximum width of a binary tree*/
+int getMaxWidth(Node* root);
+/* Helper function that allocates a new node with the
+   given data and NULL left and right pointers. */
+struct Node* newNode(int data)
+{
+  struct Node* node = (struct Node*)
+                       malloc(sizeof(struct Node));
+  node->data = data;
+  node->left = NULL;
+  node->right = NULL;
+  return(node);
+}
+/* Driver program to test size function*/
+int main()
+{
+  int t;
+  struct Node *child;
+  scanf("%d", &t);
+  while (t--)
+  {
+     map<int, Node*> m;
+     int n;
+     scanf("%d",&n);
+     struct Node *root = NULL;
+     while (n--)
+     {
+        Node *parent;
+        char lr;
+        int n1, n2;
+        scanf("%d %d %c", &n1, &n2, &lr);
+        if (m.find(n1) == m.end())
+        {
+           parent = newNode(n1);
+           m[n1] = parent;
+           if (root == NULL)
+             root = parent;
+        }
+        else
+           parent = m[n1];
+        child = newNode(n2);
+        if (lr == 'L')
+          parent->left = child;
+        else
+          parent->right = child;
+        m[n2]  = child;
+     }
+     cout << getMaxWidth(root) << endl;
+  }
+  return 0;
+}
+/* Function to get the maximum width of a binary tree*/
+int getMaxWidth(Node* root)
+{
+    if(root==NULL)
+        return 0;
+    queue<Node*> q;
+    q.push(root);
+    int max=INT_MIN;
+    while(1)
+    {
+        int nodeCount=q.size();
+        if(nodeCount>max)
+            max=nodeCount;
+        if(nodeCount==0)
+            break;
+        while(nodeCount>0)
+        {
+            Node* node=q.front();
+            q.pop();
+            if(node->left!=NULL)
+                q.push(node->left);
+            if(node->right!=NULL)
+                q.push(node->right);
+            nodeCount--;
+        }
+    }
+    return max;
+}
